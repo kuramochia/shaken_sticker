b3326a522d9a34c7dc1d100152490f857762aeda
kuramochia <kuramochia@live.jp>
2025-02-18 20:49:48 +0900

    - Renault T, Premium, Magnum 追加 - DAF XD, NGD, XF105 追加 - MAN TGX Euro5 追加

--- pack_mod/packer.bat
+++ pack_mod/packer.bat
@@ -1,7 +1,7 @@
 cd /d %~dp0
 
 SET MOD_NAME=shaken_sticker
-SET MOD_SUFFIX=_1.4
+SET MOD_SUFFIX=_1.5
 
 C:\Users\kuramochia\OneDrive\software\scs_packer\scs_packer.exe create %MOD_NAME%%MOD_SUFFIX%.scs -root .\%MOD_NAME%
 pause
